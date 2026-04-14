fbca35cce03f096d6122449c6c90f75e380e02e3
AriMusthofa <arimusthofa2005@gmail.com>
2026-04-14 08:57:59 +0800

    update v1

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = "Ari";
+$umur = 20;
+$tinggi = 175.9;
+$hobi = ["main Game", "futsal", "Membaca"];
+
+echo "Nama saya $nama, Umur saya $umur, Tinggi saya $tinggi, Hobi saya $hobi[0]"
+
+?>
