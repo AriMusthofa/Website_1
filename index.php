0e12fc7c31d29758aa7650e8d3d86ad4d8759dde
AriMusthofa <arimusthofa2005@gmail.com>
2026-05-05 08:53:40 +0800

    Pertemuan 5

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     echo "<br><button><a href='msteri1.php' Materi 1 : Variabel dan Tipe Data</a></button>";
     echo "<br><button><a href='msteri2.php' Materi 2 : Kondisi Dan Looping</a></button>";
     echo "<br><button><a href='msteri3.php' Materi 3 : Functoon</a></button>";
-    echo "<br><button><a href='msteri4.php' Materi 4 : </a></button>";
+    echo "<br><button><a href='msteri4.php' Materi 4 : Database</a></button>";
     echo "<br><button><a href='msteri5.php' Materi 5 : </a></button>";
     echo "<br><button><a href='msteri6.php' Materi 6 : </a></button>";
     echo "<br><button><a href='msteri7.php' Materi 7 : </a></button>";
---
+++ materi4.php
@@ -0,0 +1,25 @@
+<form method="POST">
+    Username:<input type="text" name="username"><br>
+    Password :<input type="password" name="password"><br>
+    Nama :<input type="text" name="nama"><br>
+    Email :<input type="text" name="email"><br>
+    <input type="submit" value="Kirim Data" name = "kirim"><br>
+</form>
+
+<?php
+include 'koneksi.php';
+if (isset($_POST['kirim'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+
+    $query = "INSERT INTO user (username, password, nama, email) VALUES ('$username', '$password', '$nama', '$email')";
+
+    if (mysqli_query($koneksi, $query)) {
+        echo "Data berhasil ditambah";
+    } else {
+        echo "Data gagal ditambahkan";
+    }
+}
+?>
